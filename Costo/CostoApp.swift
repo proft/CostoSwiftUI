97a65a0ac7b56db96013bf18ff3cab996c32b87c
proft <proftua@gmail.com>
2023-12-17 16:10:52 +0200

    Initial Commit

---
+++ Costo/CostoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CostoApp.swift
+//  Costo
+//
+//  Created by Ivan Morgun on 17.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct CostoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
